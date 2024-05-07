17da5954eb68fe6a0d98c0042ef50f53d3fe5278
Andrew <149554934+AndrewWHans@users.noreply.github.com>
2024-05-06 23:12:24 -0400

    Add files via upload

--- HashTable/account.java
+++ HashTable/account.java
@@ -1,1 +1,16 @@
+package testhash;
 
+public class account {
+
+    public int account_number;
+    public String name;
+    public double balance;
+    public account next;
+
+    public account(int account_number, String name, double balance) {
+        this.account_number = account_number;
+        this.name = name;
+        this.balance = balance;
+        this.next = null;
+    }
+}
