ac9c3697f5d0b3a27839a40e476f4c8a0180a0bc
Andrew <149554934+AndrewWHans@users.noreply.github.com>
2024-05-06 23:16:11 -0400

    Add files via upload

--- QueueTestArray/AccountNode.java
+++ QueueTestArray/AccountNode.java
@@ -1,1 +1,14 @@
+package queuetestarray;
 
+public class AccountNode {
+
+    public int account_number;
+    public String name;
+    public double balance;
+
+    public AccountNode(int v, String n, double b) {
+        this.account_number = v;
+        this.name = n;
+        this.balance = b;
+    }
+}
