1ca6f7a23476cc5b746d2af40e1566f179b1946a
Andrew <149554934+AndrewWHans@users.noreply.github.com>
2024-05-06 23:25:02 -0400

    Add files via upload

--- Stack/StackArray/AccountNode.java
+++ Stack/StackArray/AccountNode.java
@@ -1,1 +1,15 @@
+package stacktestarray;
 
+public class AccountNode
+{
+    public int account_number;
+    public String name;
+    public double balance;
+    
+    public AccountNode(int v, String n, double b)
+    {
+	this.account_number = v;
+	this.name = n;
+	this.balance = b;
+    }
+}
